dc3dc4dbffe0b4c6281e14b051529fd3a7e0c18a
Grant Custer <grantcuster@gmail.com>
2025-02-23 20:43:17 -0500

    update

--- src/routes/Dev.tsx
+++ src/routes/Dev.tsx
@@ -1,71 +1,63 @@
-import { useRef } from "react";
-import {
-  dateToReadableTime,
-  secondsToColonDuration,
-  secondsToReadableTime,
-} from "../shared/utils";
+import { useEffect, useRef, useState } from "react";
+import { secondsToColonDuration } from "../shared/utils";
 import { DataType } from "../types";
-
-const lookup = {
-  0: [0, 5923],
-  1: [5923, 12665],
-  2: [12665, 18623],
-  3: [18623, 24754],
-  4: [24754, 30596],
-  5: [30596, 36017],
-  6: [36017, 41935],
-  7: [41935, 48200],
-  8: [48200, 54051],
-  9: [54051, 60000],
-};
-const imageSideSize = 28;
-const spriteSideSize = 2048;
-
-const imagesPerSpriteSide = Math.floor(spriteSideSize / imageSideSize);
-const imagesPerSheet = imagesPerSpriteSide * imagesPerSpriteSide;
+import { CameraType, panCamera, zoomCamera } from "../shared/camera";
 
 function Dev({ data }: { data: DataType }) {
+  const [index, setIndex] = useState(0);
   const time = secondsToColonDuration(
     data.startTime + data.duration - data.currentSeconds,
   );
   const timeString = time.split(" ")[0].padStart(5, "0");
+  const [zoomContainer, setZoomContainer] = useState<HTMLDivElement | null>(
+    null,
+  );
+  const [camera, setCamera] = useState<CameraType>({ x: 0, y: 0, z: 1 });
   const timeStringArray = timeString.split("").filter((sym) => sym !== ":");
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const rows = 10;
+  const cols = 6;
+
+  const tempWidth = 56;
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d")!;
+      ctx.font = "12.5px DepartureMono";
+      ctx.textBaseline = "top";
+      const measure = ctx.measureText("h");
+      // const fontHeight =
+      //   measure.fontBoundingBoxAscent + measure.fontBoundingBoxDescent;
+      // const measureWidth = ctx.measureText(time.toString());
+      // console.log(measureWidth);
+      // const fontWidth = measure.width;
+      ctx.fillStyle = "white";
+      const y = (index % rows) * 16 + 3;
+      const x = Math.floor(index / rows) * tempWidth;
+      ctx.fillText(time.toString(), x, y);
+      setIndex(index + 1);
+    }
+  }, [canvasRef, data.currentSeconds]);
 
   return (
-    <div className="absolute inset-0 w-full h-full overflow-hidden">
-      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="relative">
-          <div
-            className="flex items-end absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            style={{
-              width: 28 * 2 * 4,
-            }}
-          >
-            {timeStringArray.map((number, i) => {
-              const [start, end] = lookup[parseInt(number)];
-              const picker =
-                Math.floor(Math.random() * (end - start)) + start;
-              const sheet = Math.floor(picker / imagesPerSheet);
-              const y = Math.floor(
-                (picker % imagesPerSheet) / imagesPerSpriteSide,
-              );
-              const x = (picker % imagesPerSheet) % imagesPerSpriteSide;
-              const multipler = 2;
-              return (
-                <div
-                  style={{
-                    width: 28 * multipler,
-                    height: 28 * multipler,
-                    backgroundImage: `url(mnist_tile_solid_${sheet}.png)`,
-                    imageRendering: "pixelated",
-                    backgroundSize: `${spriteSideSize * multipler}px ${spriteSideSize * multipler}px`,
-                    backgroundPosition: `-${imageSideSize * multipler * x}px -${imageSideSize * multipler * y}px`,
-                  }}
-                ></div>
-              );
-            })}
-          </div>
-        </div>
+    <div className="absolute inset-0 w-full h-full overflow-hidden touch-none">
+      <div
+        className="absolute left-1/2 top-1/2 transform"
+        style={{
+          transformOrigin: "0 0",
+          transform: `scale(${camera.z}) translate(-50%, -50%) translate(${camera.x}px, ${camera.y}px)`,
+        }}
+        ref={(div) => {
+          if (div) {
+            setZoomContainer(div);
+          }
+        }}
+        onClick={() => {
+          console.log("ey");
+        }}
+      >
+        <canvas ref={canvasRef} width={tempWidth * 10} height={160} />
       </div>
     </div>
   );
---
+++ src/routes/MnistUnfinished.tsx
@@ -0,0 +1,74 @@
+import { useRef } from "react";
+import {
+  dateToReadableTime,
+  secondsToColonDuration,
+  secondsToReadableTime,
+} from "../shared/utils";
+import { DataType } from "../types";
+
+const lookup = {
+  0: [0, 5923],
+  1: [5923, 12665],
+  2: [12665, 18623],
+  3: [18623, 24754],
+  4: [24754, 30596],
+  5: [30596, 36017],
+  6: [36017, 41935],
+  7: [41935, 48200],
+  8: [48200, 54051],
+  9: [54051, 60000],
+};
+const imageSideSize = 28;
+const spriteSideSize = 2048;
+
+const imagesPerSpriteSide = Math.floor(spriteSideSize / imageSideSize);
+const imagesPerSheet = imagesPerSpriteSide * imagesPerSpriteSide;
+
+function Dev({ data }: { data: DataType }) {
+  const time = secondsToColonDuration(
+    data.startTime + data.duration - data.currentSeconds,
+  );
+  const timeString = time.split(" ")[0].padStart(5, "0");
+  const timeStringArray = timeString.split("").filter((sym) => sym !== ":");
+
+  return (
+    <div className="absolute inset-0 w-full h-full overflow-hidden">
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="relative">
+          <div
+            className="flex items-end absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            style={{
+              width: 28 * 2 * 4,
+            }}
+          >
+            {timeStringArray.map((number, i) => {
+              const [start, end] = lookup[parseInt(number)];
+              const picker =
+                Math.floor(Math.random() * (end - start)) + start;
+              const sheet = Math.floor(picker / imagesPerSheet);
+              const y = Math.floor(
+                (picker % imagesPerSheet) / imagesPerSpriteSide,
+              );
+              const x = (picker % imagesPerSheet) % imagesPerSpriteSide;
+              const multipler = 2;
+              return (
+                <div
+                  style={{
+                    width: 28 * multipler,
+                    height: 28 * multipler,
+                    backgroundImage: `url(mnist_tile_solid_${sheet}.png)`,
+                    imageRendering: "pixelated",
+                    backgroundSize: `${spriteSideSize * multipler}px ${spriteSideSize * multipler}px`,
+                    backgroundPosition: `-${imageSideSize * multipler * x}px -${imageSideSize * multipler * y}px`,
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dev;
